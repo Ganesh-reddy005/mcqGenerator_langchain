377f2c5bc51e0704bf4c02835d98dad3b59a4fd9
Ganesh Reddy <b.ganes.reddy.05@gmail.com>
2025-08-05 20:06:58 +0530

    done with experminting

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 #installing local packages
+
+from setuptools import find_packages,setup
+setup(
+    name='mcq_generator',
+    version='0.0.1',
+    author='Ganesh reddy',
+    author_email='b.ganesh.reddy.05@gmail.com',
+    install_requires=["langchain_openai","langchain","python-dotenv","streamlit","PyPDF2"],
+    packages=find_packages()
+)
